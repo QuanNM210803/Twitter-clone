8bca9bbd80bd91a9c779bea942a0b59ebe797332
quan <nnmhqn2003@gmail.com>
2024-04-27 11:50:45 +0700

    eq

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -17,6 +17,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'react/prop-types':'off',
     //Common
     'no-useless-catch': 0,
     'no-lonely-if': 0,
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,23 @@ import { Route, Routes } from 'react-router-dom'
 import LoginPage from './pages/auth/login/LoginPage'
 import SignUpPage from './pages/auth/signup/SignUpPage'
 import HomePage from './pages/home/HomePage'
+import Sidebar from './components/common/SideBar'
+import RightPanel from './components/common/RightPanel'
+import NotificationPage from './pages/notification/NotificationPage'
+import ProfilePage from './pages/profile/ProfilePage'
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar/>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/notifications" element={<NotificationPage/>}/>
+        <Route path="/profile/:username" element={<ProfilePage/>}/>
       </Routes>
+      <RightPanel/>
     </div>
   )
 }
